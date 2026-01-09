7eb1d040ceb4dcec0f8904abfe530e2e4d6fb1bd
Katarina Vujovic <v.katar551@gmail.com>
2026-01-09 17:31:41 +0100

    init: Backend

---
+++ jutjubic-be/src/test/java/com/jutjubiccorps/jutjubic/JutjubicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jutjubiccorps.jutjubic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JutjubicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
